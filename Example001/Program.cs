f2af5620f6e7e95368695b164c0f10b261556dee
MariaGee <masha.avramchencko@yandex.com>
2022-08-19 22:57:13 +0300

    Add Example 001

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5      782 -> 8      918 -> 1
+
+int number = new Random().Next(100,1000);
+Console.WriteLine("Было заданно число: " + number);
+
+int SecondNumber (int count)
+{
+    int index = count / 10 % 10;
+    return index;
+}
+
+int result = SecondNumber(number);
+Console.WriteLine("Вторая цифра в числе: " + result);
+
