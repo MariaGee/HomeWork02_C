80d345192d9994e9dcc35a0136cda52866ee2f1f
MariaGee <masha.avramchencko@yandex.com>
2022-08-19 23:47:34 +0300

    Add Example 002

---
+++ Example002/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5      78 -> третьей цифры нет     32679 -> 6
+
+int number = new Random().Next(1, 1000000);
+Console.WriteLine("Было заданно число: " + number);
+
+string numberString = Convert.ToString(number);
+
+if (numberString.Length < 3) Console.WriteLine("У числа нет третьей цифры");
+else Console.WriteLine("Третье число: " + numberString[2]);
